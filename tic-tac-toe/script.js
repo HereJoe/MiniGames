819807066549d9d838b61d87eee9ce252f3db5f5
herejoe <herejoe@qq.com>
2024-03-05 23:15:12 +1030

    winner squares turn green

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -2,53 +2,59 @@ class Square extends React.Component {
   render() {
     const { value, isOccupied, iswinner } = this.props;
     const className = `square ${iswinner ? "winner" : ""} ${
-    isOccupied ? "occupied" : ""
+      isOccupied ? "occupied" : ""
     }`;
-    return /*#__PURE__*/(
-      React.createElement("button", { className: className, onClick: () => this.props.onClick() },
-      this.props.value));
-
-
-  }}
-
+    return /*#__PURE__*/ React.createElement(
+      "button",
+      { className: className, onClick: () => this.props.onClick() },
+      this.props.value
+    );
+  }
+}
 
 class Board extends React.Component {
   renderSquare(i) {
-    const { squares, onClick, winner, winInd, occupied } = this.props;
-    return /*#__PURE__*/(
-      React.createElement(Square, {
-        value: squares[i],
-        isOccupied: occupied[i],
-        iswinner: winInd && winInd.includes(i),
-        onClick: () => {
-          onClick(i);
-        } }));
-
-
+    const { squares, onClick, winner, winInd, occupied } = this.props;	
+    return /*#__PURE__*/ React.createElement(Square, {
+      value: squares[i],
+      isOccupied: occupied[i],
+      iswinner: winInd && winInd.includes(i),
+      onClick: () => {
+        onClick(i);
+      }
+    });
   }
 
   render() {
-    return /*#__PURE__*/(
-      React.createElement("div", null, /*#__PURE__*/
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(0),
-      this.renderSquare(1),
-      this.renderSquare(2)), /*#__PURE__*/
+    return /*#__PURE__*/ React.createElement(
+      "div",
+      null /*#__PURE__*/,
+      React.createElement(
+        "div",
+        { className: "board-row" },
+        this.renderSquare(0),
+        this.renderSquare(1),
+        this.renderSquare(2)
+      ) /*#__PURE__*/,
 
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(3),
-      this.renderSquare(4),
-      this.renderSquare(5)), /*#__PURE__*/
+      React.createElement(
+        "div",
+        { className: "board-row" },
+        this.renderSquare(3),
+        this.renderSquare(4),
+        this.renderSquare(5)
+      ) /*#__PURE__*/,
 
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(6),
-      this.renderSquare(7),
-      this.renderSquare(8))));
-
-
-
-  }}
-
+      React.createElement(
+        "div",
+        { className: "board-row" },
+        this.renderSquare(6),
+        this.renderSquare(7),
+        this.renderSquare(8)
+      )
+    );
+  }
+}
 
 class Game extends React.Component {
   constructor(props) {
@@ -59,11 +65,11 @@ class Game extends React.Component {
       winInd: null,
       occupied: Array(9).fill(false),
       history: [
-      {
-        squares: Array(9).fill(null) }] };
-
-
-
+        {
+          squares: Array(9).fill(null)
+        }
+      ]
+    };
   }
   vibrate(i) {
     let occupied = this.state.occupied.slice();
@@ -77,27 +83,27 @@ class Game extends React.Component {
   }
   calcWinner(squares) {
     const seqs = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]];
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
 
     for (let i = 0; i < seqs.length; i++) {
       const [a, b, c] = seqs[i];
       if (
-      squares[a] &&
-      squares[a] === squares[b] &&
-      squares[a] === squares[c])
-      {
-        this.state.winInd = seqs[i];
-        return squares[a];
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return { winner: squares[a], winInd: seqs[i] };
       }
     }
-    return null;
+    return { winner: null, winInd: null };
   }
   handleClick(i) {
     const his = this.state.history;
@@ -109,24 +115,22 @@ class Game extends React.Component {
 
     const squares = cur.squares.slice();
     squares[i] = this.state.xIsNext ? "X" : "O";
-    const winner = this.calcWinner(squares);
+    let { winner, winInd } = this.calcWinner(squares);
     this.setState({
-      history: his.concat([
-      {
-        squares: squares }]),
-
-
+      history: his.concat([{ squares: squares }]),
       xIsNext: !this.state.xIsNext,
-      winner: winner });
-
+      winner: winner,
+      winInd: winInd
+    });
   }
 
   jumpTo(step) {
     this.setState({
       winner: null,
+      winInd: null,
       history: this.state.history.slice(0, step + 1),
-      xIsNext: step % 2 === 0 });
-
+      xIsNext: step % 2 === 0
+    });
   }
 
   render() {
@@ -148,35 +152,43 @@ class Game extends React.Component {
         return null;
       }
       const desc = i ? "Go to step #" + i : "Go to game start";
-      return /*#__PURE__*/(
-        React.createElement("li", { key: "li" + i }, /*#__PURE__*/
-        React.createElement("button", { className: "move-button", onClick: () => this.jumpTo(i) },
-        desc)));
-
-
-
+      return /*#__PURE__*/ React.createElement(
+        "li",
+        { key: "li" + i } /*#__PURE__*/,
+        React.createElement(
+          "button",
+          { className: "move-button", onClick: () => this.jumpTo(i) },
+          desc
+        )
+      );
     });
-    return /*#__PURE__*/(
-      React.createElement("div", { className: "game" }, /*#__PURE__*/
-      React.createElement("div", { className: "game-board" }, /*#__PURE__*/
-      React.createElement(Board, {
-        squares: cur.squares,
-        occupied: occupied,
-        xIsNext: xIsNext,
-        winInd: winInd,
-        onClick: i => this.handleClick(i) })), /*#__PURE__*/
+    return /*#__PURE__*/ React.createElement(
+      "div",
+      { className: "game" } /*#__PURE__*/,
+      React.createElement(
+        "div",
+        { className: "game-board" } /*#__PURE__*/,
+        React.createElement(Board, {
+          squares: cur.squares,
+          occupied: occupied,
+          xIsNext: xIsNext,
+          winInd: winInd,
+          onClick: (i) => this.handleClick(i)
+        })
+      ) /*#__PURE__*/,
 
-
-      React.createElement("div", { className: "game-info" }, /*#__PURE__*/
-      React.createElement("div", null, status), /*#__PURE__*/
-      React.createElement("ol", null, moves))));
-
-
-
-  }}
-
+      React.createElement(
+        "div",
+        { className: "game-info" } /*#__PURE__*/,
+        React.createElement("div", null, status) /*#__PURE__*/,
+        React.createElement("ol", null, moves)
+      )
+    );
+  }
+}
 
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render( /*#__PURE__*/React.createElement(Game, null));
+root.render(/*#__PURE__*/ React.createElement(Game, null));
+	
